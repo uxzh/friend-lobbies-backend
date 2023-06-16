132768f66e6cbb50aa9989cc250c9d090f0839b5
Ohad <ohad.licht@gmail.com>
2023-06-16 13:12:38 +0300

    remove Number from id calls, as uuid is a string

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const UsersDAO = require('../DAO/users.dao')
 class userController{
     static async getSingle(req, res, next){
         try{
-            const user = await UsersDAO.getById(Number(req.userID));
+            const user = await UsersDAO.getById(req.userID);
             return res.ok(user);
         }catch(err){
             res.status(500).send(err)
@@ -11,7 +11,7 @@ class userController{
 
     static async update(req, res, next){
         try{
-            update = await UsersDAO.update(Number(req.userID), req.body);
+            update = await UsersDAO.update(req.userID, req.body);
             return res.ok("User updated")
         }catch(err){
             res.status(500).send(err)
@@ -29,7 +29,7 @@ class userController{
 
     static async ban(req, res, next){
         try{
-            await UsersDAO.update(Number(req.params.id), {isBanned: true});
+            await UsersDAO.update(req.params.id, {isBanned: true});
             return res.ok("Banned user")
         }catch(err){
             res.status(500).send(err)
@@ -38,10 +38,10 @@ class userController{
 
     static async mute(req, res, next){
         try{
-            const user = await UsersDAO.getById(Number(req.userID));
+            const user = await UsersDAO.getById(req.userID);
             const mutes = user.mutes;
             mutes.push({user:req.params.id, lobby: req.params.lobby});
-            await UsersDAO.update(Number(req.userID), {mutes});
+            await UsersDAO.update(req.userID, {mutes});
             return res.ok("Muted user");
         }catch(err){
             res.status(500).send(err)
@@ -59,10 +59,10 @@ class userController{
 
     static async saveLobby(req, res, next){
         try{
-            const user = await UsersDAO.getById(Number(req.userID));
+            const user = await UsersDAO.getById(req.userID);
             const saved = user.savedLobbies;
             saved.push(req.params.id);
-            await UsersDAO.update(Number(req.userID), {savedLobbies: saved});
+            await UsersDAO.update(req.userID, {savedLobbies: saved});
             return res.ok("Saved lobby")
         }catch(err){
             res.status(500).send(err)
